bd0d42c6a59fc5ff7e42c1a77a4f9c6b6b6710a0
abbilovesfood <42784774+abbilovesfood@users.noreply.github.com>
2018-08-28 14:56:57 -0600

    Update data-types.py

--- unit1/data-types.py
+++ unit1/data-types.py
@@ -16,23 +16,41 @@ multiline comment
 
 # *** DATA TYPE: INTEGER ***
 # TODO: An integer number (no decimal)
-
+integer = 5
+print(integer)
+print(type(integer))
 
 # *** DATA TYPE: FLOAT ***
 # TODO: A decimal number
-
+decimal = 6.794
+print(decimal)
+print(type(decimal))
 
 # *** DATA TYPE: STRING ***
 # TODO: A string of characters enclosed in quotes
+text = "running"
+print(text)
+print(type(text))
 
 
 # *** TYPE CASTING ***
 # This converts one type to another
 
 # TODO: Cast float to int
+float_num = 5.259
+int_num = int(float_num)
+print(int_num)
+print(type(int_num))
 
 # TODO: Cast int to string
+number = "78"
+print("5 " + str(number))
 
 # TODO: Cast number string to int
+number = "5" 
+print(int(number) + 2) 
 
 # TODO: Input demo (str to float)
+print("give me a decimal and ill add 1 to it")
+number = float(input())
+print(number + 1)
